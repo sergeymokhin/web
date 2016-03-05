4c83504e67c7c0e38eb9907ea81f84094bf7f680
sergeymokhin <sergey.mokhin@gmail.com>
2016-03-05 15:33:47 +0600

    changing files

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+#sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+udo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
