8057b0efe79e71062a21f6c55beacb2ad83c1096
sergeymokhin <sergey.mokhin@gmail.com>
2016-03-05 15:36:13 +0600

    add letter

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-udo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
