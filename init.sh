53a296c1c4cbd784d33fbb4c87cbc1adbc94e7ce
sergeymokhin <sergey.mokhin@gmail.com>
2016-03-06 09:05:40 +0600

    bug fix

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo rm /etc/nginx/sites-enabled/default
+#sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
