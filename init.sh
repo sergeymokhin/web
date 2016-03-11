c551cd88595220e3ac09b91e812b16443c302aa3
sergeymokhin <sergey.mokhin@gmail.com>
2016-03-11 07:34:16 +0600

    lets try 3

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
+# Nginx
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+
+#Gunicorn
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test.conf
 sudo /etc/init.d/gunicorn restart
