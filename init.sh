e89431e29367bdb46967f223004c04d1d987739b
sergeymokhin <sergey.mokhin@gmail.com>
2016-03-13 08:30:14 +0600

    add rm default to init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 # Nginx
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 #Gunicorn
