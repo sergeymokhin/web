60bae00448ba4241cc690f3de451e1473b67e7aa
sergeymokhin <sergey.mokhin@gmail.com>
2016-03-15 13:32:07 +0600

    url patterns

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url, patterns
 from django.contrib import admin
 admin.autodiscover()
 
+'''
 urlpatterns = [  
     url(r'^$', 'qa.views.test'),
     url(r'^login/.*$', 'qa.views.test'),
@@ -11,3 +12,13 @@ urlpatterns = [
     url(r'^popular/.*$', 'qa.views.test'),
     url(r'^new/.*$', 'qa.views.test')
 ]
+'''
+urlpatterns = patterns('qa.views',                                              
+   url(r'^$', 'test'),                                                              
+   url(r'^login/.*$', 'test', name='login'),                                    
+   url(r'^signup/.*', 'test', name='signup'),                                   
+   url(r'^question/(?P<id>[0-9]+)/$', 'test', name='question'),                 
+   url(r'^ask/.*', 'test', name='ask'),                                         
+   url(r'^popular/.*', 'test', name='popular'),                                 
+   url(r'^new/.*', 'test', name='new'),                                         
+)    
