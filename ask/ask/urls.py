32633c63e01f71d3e652b13f446435866b83a6ba
sergeymokhin <sergey.mokhin@gmail.com>
2016-03-16 07:15:57 +0600

    add second urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,24 +1,8 @@
-from django.conf.urls import include, url, patterns 
+from django.conf.urls import patterns, include, url
+
 from django.contrib import admin
 admin.autodiscover()
 
-'''
-urlpatterns = [  
-    url(r'^$', 'qa.views.test'),
-    url(r'^login/.*$', 'qa.views.test'),
-    url(r'^signup/.*$', 'qa.views.test'),
-    url(r'^question/\d+/$', 'qa.views.test'),
-    url(r'^ask/.*$', 'qa.views.test'),
-    url(r'^popular/.*$', 'qa.views.test'),
-    url(r'^new/.*$', 'qa.views.test')
-]
-'''
-urlpatterns = patterns('qa.views',                                              
-   url(r'^$', 'test'),                                                              
-   url(r'^login/.*$', 'test', name='login'),                                    
-   url(r'^signup/.*', 'test', name='signup'),                                   
-   url(r'^question/(?P<id>[0-9]+)/$', 'test', name='question'),                 
-   url(r'^ask/.*', 'test', name='ask'),                                         
-   url(r'^popular/.*', 'test', name='popular'),                                 
-   url(r'^new/.*', 'test', name='new'),                                         
-)    
+urlpatterns = patterns('',
+    url(r'^', include('qa.urls')),
+)
